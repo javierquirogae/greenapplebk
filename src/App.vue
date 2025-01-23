2a2c136206f2ff9dd6301a86a309e7b623e91e63
Jacob Quiroga <jacobquiroga01@gmail.com>
2025-01-23 13:26:44 -0500

    text

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
       <div class="flex justify-between items-center">
         <div>
           <h1 class="text-3xl font-bold">Green Apple Bookkeeping</h1>
-          <p>Growing yor business one transaction at a time.</p>
+          <p>Growing your business one transaction at a time.</p>
         </div>
-        <nav>
+        <nav class="hidden md:block">
           <ul class="flex space-x-6">
             <li><a href="#home" class="hover:text-lime-300">Home</a></li>
             <li><a href="#services" class="hover:text-lime-300">Services</a></li>
@@ -19,21 +19,24 @@
     </header>
 
     <!-- Home Section -->
-    <section id="home" class="text-center py-16">
-      <h2 class="text-4xl font-semibold text-lime-500">Welcome to Green Apple Bookkeeping</h2>
-      <p class="text-lg text-lime-500 mt-4">
-        We provide expert bookkeeping services for small businesses, startups, and individuals.
-      </p>
+    <section id="home" class="text-center py-16 px-10">
+      <h2 class="text-4xl font-semibold text-lime-500">Welcome!</h2>
       <div class="flex justify-center items-center">
         <img
           src="https://javierquirogae.github.io/pics/pics/GA_LOGO.png"
           alt="logo"
-          class="w-1/4"
+          class="w-full sm:w-1/2 md:w-1/3 lg:1/4"
         />
       </div>
+      <p class="text-lg text-lime-500 px-5 sm:px-10 md:px-20 lg:px-200 text-justify">
+        Saving service-based business owners time and money by creating, organizing, and maintaining
+        financial systems related to bookkeeping to ensure compliance, and unpacking financial
+        reports for understanding.
+      </p>
+
       <button
         @click="contactUs"
-        class="bg-lime-500 text-white px-6 py-3 mt-6 rounded-lg hover:bg-lime-600 transition"
+        class="bg-lime-500 text-white px-6 py-3 mt-3 rounded-lg hover:bg-lime-600 transition"
       >
         Get in Touch
       </button>
@@ -41,47 +44,114 @@
 
     <!-- Services Section -->
     <section id="services" class="py-16 px-4 text-center">
-      <h2 class="text-4xl font-semibold text-lime-500 mb-8">Our Services</h2>
+      <h2 class="text-4xl font-semibold text-lime-500 mb-8">Services</h2>
       <div class="grid grid-cols-1 gap-8">
         <div class="group bg-white p-6 rounded-lg shadow-md relative">
+          <h3 class="text-2xl font-semibold text-lime-500 cursor-pointer">Clean-Up/Catch-Up</h3>
+          <p
+            class="text-lime-500 hidden group-hover:block px-5 sm:px-10 md:px-20 lg:px-200 text-justify"
+          >
+            Has bookkeeping taken a back seat in your busy schedule? Our Clean-Up and Catch-Up
+            services are designed to get your financial records back on track. Whether you're
+            preparing for tax season, need to reconcile months of transactions, or want to clean up
+            your QuickBooks file, we ensure your books are accurate and up-to-date. With a
+            nonjudgmental approach and a commitment to excellence, we'll organize your financials
+            and provide you with a clear picture of your business's financial health, paving the way
+            for growth and balance.
+          </p>
+        </div>
+        <div class="group bg-white p-6 rounded-lg shadow-md relative">
           <h3 class="text-2xl font-semibold text-lime-500 cursor-pointer">Monthly Bookkeeping</h3>
-          <p class="text-lime-500 mt-4 hidden group-hover:block">
-            Accurate and timely monthly bookkeeping to keep your business finances organized.
+          <p
+            class="text-lime-500 hidden group-hover:block px-5 sm:px-10 md:px-20 lg:px-200 text-justify"
+          >
+            Stay on top of your business finances with our Monthly Bookkeeping service. We provide
+            comprehensive support, including recording and categorizing transactions based on your
+            unique needs, reconciling all bank and credit card accounts, and reviewing and analyzing
+            monthly financial reports such as the income statement, balance sheet, and statement of
+            cash flows. To keep you informed, we deliver financial reports and highlights of our
+            analysis through either a live Zoom call or a prerecorded video. Our tailored approach
+            ensures you have the clarity and insights needed to make strategic business decisions
+            while freeing up your time to focus on what you do best.
           </p>
         </div>
-
         <div class="group bg-white p-6 rounded-lg shadow-md relative">
-          <h3 class="text-2xl font-semibold text-lime-500 cursor-pointer">Tax Preparation</h3>
-          <p class="text-lime-500 mt-4 hidden group-hover:block">
-            We handle tax filing for both individuals and small businesses to ensure you stay
-            compliant.
+          <h3 class="text-2xl font-semibold text-lime-500 cursor-pointer">
+            Digitized Source Document Storage
+          </h3>
+          <p
+            class="text-lime-500 hidden group-hover:block px-5 sm:px-10 md:px-20 lg:px-200 text-justify"
+          >
+            Say goodbye to piles of paperwork and hello to organized digital archives. Our Digitized
+            Source Document Storage service provides a secure and efficient way to store your
+            financial documents. We'll organize and store bank statements from all banks and credit
+            cards in a cloud-based folder, giving you easy access in the unlikely event of an
+            audit—no need to wait on your bank or handle the process yourself. Additionally, we'll
+            digitize receipts, ensuring they won't fade like physical copies and are always
+            accessible when needed. By going digital, you'll save time, reduce clutter, and have
+            everything you need at your fingertips for tax time or audits, bringing balance and
+            simplicity to your business operations.
           </p>
         </div>
-
         <div class="group bg-white p-6 rounded-lg shadow-md relative">
-          <h3 class="text-2xl font-semibold text-lime-500 cursor-pointer">Financial Reporting</h3>
-          <p class="text-lime-500 mt-4 hidden group-hover:block">
-            Get detailed financial reports to make informed decisions about your business's future.
+          <h3 class="text-2xl font-semibold text-lime-500 cursor-pointer">
+            Virtual Financial Reporting
+          </h3>
+          <p
+            class="text-lime-500 hidden group-hover:block px-5 sm:px-10 md:px-20 lg:px-200 text-justify"
+          >
+            Understanding your financial performance is key to achieving your business goals. Our
+            Financial Reporting service provides customized reports tailored to your needs. Choose
+            between a face-to-face Zoom meeting for a more personal discussion or a detailed email
+            with a video walkthrough and analysis of your reports. We'll highlight trends, pinpoint
+            concerns, and identify areas for growth, empowering you with actionable insights.
+            Whether you're looking to improve efficiency, plan for growth, or simply gain clarity,
+            Green Apple Bookkeeping ensures financial transparency and understanding every step of
+            the way.
           </p>
         </div>
-
         <div class="group bg-white p-6 rounded-lg shadow-md relative">
-          <h3 class="text-2xl font-semibold text-lime-500 cursor-pointer">Consulting & Advisory</h3>
-          <p class="text-lime-500 mt-4 hidden group-hover:block">
-            Our expert consultants provide personalized financial advice to help you grow and manage
-            your business.
+          <h3 class="text-2xl font-semibold text-lime-500 cursor-pointer">
+            Digital Tax Prep Package
+          </h3>
+          <p
+            class="text-lime-500 hidden group-hover:block px-5 sm:px-10 md:px-20 lg:px-200 text-justify"
+          >
+            Simplify tax season with our Digital Tax Prep Package. This service provides your tax
+            preparer or accountant with all the necessary documents and reports they need, all
+            organized in a tax-ready folder. From bank statements and receipts to categorized
+            transaction records and financial reports, we'll ensure everything is ready to go just
+            in time for tax season. Say goodbye to the stress of gathering documents and let us
+            streamline the process for you, saving you time and ensuring accuracy.
           </p>
         </div>
       </div>
     </section>
 
     <!-- About Us Section -->
-    <section id="about" class="py-16 text-center">
-      <h2 class="text-4xl font-semibold text-lime-500 mb-6">About Us</h2>
-      <p class="text-lg text-lime-500 justify-center items-center">
-        Green Apple Bookkeeping was founded to simplify financial processes for small businesses and
-        individuals. Our team of experienced bookkeepers and financial experts ensures that you can
-        focus on growing your business while we take care of your books.
+    <section id="about" class="py-16 text-center px-10">
+      <h2 class="text-4xl font-semibold text-lime-500 mb-6">About Green Apple Bookkeeping</h2>
+      <p class="text-lg text-lime-500 px-5 sm:px-10 md:px-20 lg:px-200 text-justify">
+        Did you know small business owners often spend 4-6 hours a week on bookkeeping and
+        administrative tasks? That's about 200-300 hours a year—time you could be using to focus on
+        what you love. Imagine what you could do with those extra hours: spend more time with
+        family, take a long-awaited vacation, or invest in growing your business. Let me help you
+        reclaim that time by handling your bookkeeping needs. At Green Apple Bookkeeping, we blend
+        professionalism with approachability to empower small business owners with organized and
+        accurate financial systems. Drawing from a background in education, I specialize in putting
+        complex accounting practices into clear, understandable language. Just as an educator sets
+        students up for success, I take the time to set up efficient, tailored systems that help
+        businesses thrive. By getting to know my clients and their goals, I implement bookkeeping
+        solutions that save time, bring clarity, and enable growth—both in business and in personal
+        life. With monthly and quarterly reporting, I provide insights into your financial trends,
+        helping you make informed decisions that boost profitability and optimize cash flow. And
+        when tax season arrives, your books will be in order, making the process smoother, more
+        cost-effective, and less stressful. Organization is key to success, and I ensure every
+        transaction has a place, so you and your financial team can access the right information
+        when you need it. Whether it's clean-ups, catch-ups, monthly categorization and
+        reconciliations, or financial reporting, I'll make sure everything is where it should be.
+        Choosing my services means more than just getting a bookkeeper; it means gaining a partner
+        committed to your growth and success.
       </p>
     </section>
 
